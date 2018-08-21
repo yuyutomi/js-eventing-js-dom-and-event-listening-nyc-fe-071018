81614d826f0aab9640c869e2199c7b69e81126ee
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-21 23:21:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ main.addEventListener('click', (event) => {
   console.log("I was clicked");
 });
 paragraph.addEventListener('click', (event) => {
-  alert('You clicked teh p!')
+  alert('You clicked the p!')
   paragraph.style.backgroundColor = 'yellow'
 })

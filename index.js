f05e0c9a7a1229734649f8eaa61e8647b0e79368
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-21 23:26:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const paragraph = document.querySelector('p');
+const paragraph = document.querySelector('p')
+
+// can also do things like: 
+//takes the first p in a div --> const paragraph = document.querySelector('div > p');
+//all paragraphs in a div --> const paragraph = document.querySelector('div > p');
 
 paragraph.addEventListener('click', (event) => {
   console.log("I was clicked")

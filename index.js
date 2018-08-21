344c5bccc8fbe72764998429f87f8a8764a14efb
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-21 23:21:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const main = document.querySelector('#main');
+const paragraph = document.querySelector('p');
 // can also do things like: 
 //takes the first p in a div --> const paragraph = document.querySelector('div > p');
 //all paragraphs in a div --> const paragraph = document.querySelector('div > p');

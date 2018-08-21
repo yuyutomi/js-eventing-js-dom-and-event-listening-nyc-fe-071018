dfec19ba36ed9e6eeb520738e39c96ae0db1dc0e
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-21 23:23:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const paragraph = document.querySelector('p');
 paragraph.addEventListener('click', (event) => {
   console.log("I was clicked")
 })
+
 paragraph.addEventListener('click', (event) => {
   alert('You clicked the p!')
   paragraph.style.backgroundColor = 'yellow'

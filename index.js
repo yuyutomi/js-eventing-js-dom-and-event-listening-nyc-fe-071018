118ef1890dde6186c9ae3aa5008fe7f2ccd5ff6c
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-21 23:27:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const paragraph = document.querySelector('p')
 //takes the first p in a div --> const paragraph = document.querySelector('div > p');
 //all paragraphs in a div --> const paragraph = document.querySelector('div > p');
 
-paragraph.addEventListener('click', (event) => {
-  console.log("I was clicked")
-})
 
 paragraph.addEventListener('click', (event) => {
   alert('You clicked the p!')
   paragraph.style.backgroundColor = 'yellow'
+})
+paragraph.addEventListener('click', (event) => {
+  console.log("I was clicked")
 })

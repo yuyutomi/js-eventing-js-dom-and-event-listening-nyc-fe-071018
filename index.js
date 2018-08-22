e97788a6c0eed0c16c4afe4625fa57356944f86f
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-22 00:24:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,34 @@
+const main = document.querySelector('#main')
+const paragraph = document.querySelector('p')
+const input = document.querySelector('input')
+
+main.addEventListener('click', (event) => {
+  console.log("I was clicked")
+})
+
+paragraph.addEventListener('click', (event) => {
+  alert('You clicked the p!')
+  paragraph.style.backgroundColor = 'yellow'
+})
+
+
+// can also do things like: 
+//takes the first p in a div --> const paragraph = document.querySelector('div > p');
+//all paragraphs in a div --> const paragraph = document.querySelector('div > p');
+
+
+let divs = document.querySelectorAll('div')
+function bubble(event) {
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled')
+}
+
+for (div of divs) {
+  div.addEventListener('click', bubble)
+}
+
+input.addEventListener('keydown', (event) => {
+  if (event.which === 71) {
+    event.preventDefault()
+  }
+  console.log(event)
+})
